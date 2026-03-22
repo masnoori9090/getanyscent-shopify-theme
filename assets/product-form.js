6f0fe929cd616ce42a64d93756578d4c90b1419d
mohammadmansoori <130860651+masnoori9090@users.noreply.github.com>
2026-03-23 00:35:41 +0400

    fix: restore cart drawer behavior and notification layout

--- assets/product-form.js
+++ assets/product-form.js
@@ -8,7 +8,7 @@ if (!customElements.get('product-form')) {
         this.form = this.querySelector('form');
         this.variantIdInput.disabled = false;
         this.form.addEventListener('submit', this.onSubmitHandler.bind(this));
-        this.cart = document.querySelector('cart-notification') || document.querySelector('cart-drawer');
+        this.cart = document.querySelector('cart-drawer') || document.querySelector('cart-notification');
         this.submitButton = this.querySelector('[type="submit"]');
         this.submitButtonText = this.submitButton.querySelector('span');
 
